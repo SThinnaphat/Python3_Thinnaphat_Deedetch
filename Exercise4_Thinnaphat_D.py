5be2bec732166de99a5d0bedf68e5ef3677c3002
SThinnaphat <SThinnaphat>
2020-06-12 15:18:26 +0700

    Create Exercise4_Thinnaphat_D.py
    Exercise4

---
+++ Exercise4_Thinnaphat_D.py
@@ -0,0 +1,10 @@
+FEScore = 50
+GBScore = 65.87
+ICScore = 79.25
+CPScore = 56.89
+
+print("---Your Score---")
+print("Foundation English :",FEScore)
+print("General Business :",GBScore)
+print("Introduction to Computer System :",ICScore)
+print("Computer Programming:",CPScore)
