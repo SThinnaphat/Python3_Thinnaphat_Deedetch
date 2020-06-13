fc285375c39fe29465cceb23e86cc8820a208082
SThinnaphat <SThinnaphat>
2020-06-13 23:03:09 +0700

    Create Exercise5_1_Thinnaphat_D.py
    Basic Calculation btw 2 Numbers

---
+++ Exercise5_1_Thinnaphat_D.py
@@ -0,0 +1,7 @@
+x = int(input("Input Your First digit"))
+y = int(input("Input Your Second digit"))
+print(x ,"+" ,y ,"=" ,x+y)
+print(x ,"-" ,y ,"=" ,x-y)
+print(x ,"*" ,y ,"=" ,x*y)
+print(x ,"/" ,y ,"=" ,x/y)
+
