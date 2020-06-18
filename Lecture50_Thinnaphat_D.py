1fdf2fe94986673e7c6c3d64512b5266c818f074
SThinnaphat <SThinnaphat>
2020-06-18 22:58:45 +0700

    Create Lecture50_Thinnaphat_D.py
    Function for Calculation

---
+++ Lecture50_Thinnaphat_D.py
@@ -0,0 +1,13 @@
+def AddNumber(x,y):
+    print(x+y)
+def MinusNumber(x,y):
+    print(x-y)
+def MultiplyNumber(x,y):
+    print(x*y)
+def DivideNumber(x,y):
+    print(x/y)
+
+AddNumber(10,15)
+MinusNumber(20,8)
+MultiplyNumber(20,5)
+DivideNumber(10,3)
