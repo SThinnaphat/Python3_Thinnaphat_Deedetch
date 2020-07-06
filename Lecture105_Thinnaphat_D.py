96fc417624e4b45ec9e3388f83879fa9c950cd0b
SThinnaphat <SThinnaphat>
2020-07-07 01:01:44 +0700

    Create Lecture105_Thinnaphat_D.py
    Object (Inheritance)

---
+++ Lecture105_Thinnaphat_D.py
@@ -0,0 +1,27 @@
+class Vehicle:
+    licenseCode = ""
+    serialCode = ""
+    face = ""
+    def turnOnAirConditioner(self):
+        print("Turn On : Air")
+class Car(Vehicle):
+    def sayHello(self):
+        print("HelloWorld")
+class PickUp(Vehicle):
+    pass
+class Van(Vehicle):
+    pass
+class EstateCar(Vehicle):
+    pass
+
+car1 = Car()
+car1.turnOnAirConditioner()
+
+pickup1 = PickUp()
+pickup1.turnOnAirConditioner()
+
+van1 = Van()
+van1.turnOnAirConditioner()
+
+EstateCar1 = EstateCar()
+EstateCar1.turnOnAirConditioner()
