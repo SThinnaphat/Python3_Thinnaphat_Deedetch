e51f0c63150e802bb683e513cbe381b1220d0108
SThinnaphat <65947201+SThinnaphat@users.noreply.github.com>
2021-05-28 23:29:20 +0700

    Create howto_current_date_time.py

---
+++ howto_current_date_time.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
